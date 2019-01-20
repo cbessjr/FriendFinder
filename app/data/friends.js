f418414466205756f16e65feaed2e7cae7eb543f
Ender <cbessjr@gmail.com>
2019-01-20 09:58:03 -0700

    012019. Updated files.

---
+++ app/data/friends.js
@@ -0,0 +1,25 @@
+var friends = [
+    {
+        name: "Snoopy",
+        photo: "https://clipartion.com/wp-content/uploads/2015/12/snoopy-clip-art-free.jpeg",
+        scores: []
+    },
+    {
+        name: "Charlie Brown",
+        photo: "https://tse1.mm.bing.net/th?id=OIP.ip_hgCxlm-Tt_tWPd4ritQHaH9&pid=Api&w=331&h=356&rs=1&p=0",
+        scores: []
+    },
+    {
+        name: "Linus",
+        photo: "https://s-media-cache-ak0.pinimg.com/564x/38/86/21/388621da72e47eef926d6dd09161c03a.jpg",
+        scores: []
+    },
+    {
+        name: "Franklin",
+        photo: "https://tse2.mm.bing.net/th?id=OIP.esZp5K-rfIouTIZCq-7pmgHaMb&pid=Api",
+        scores: []
+    },
+
+];
+
+module.exports = friends;
