c137098790cfc7f5f77bd8fe87cbac2231930ed4
Ender <cbessjr@gmail.com>
2019-01-20 19:02:40 -0700

    012019. Updated files.

--- server.js
+++ server.js
@@ -1,13 +1,40 @@
 // Dependencies
-// =============================================================
-var express = require("express");
-var path = require("path");
+// var express = require("express");
+// var path = require("path");
 
-// Sets up the Express App
-// =============================================================
-var app = express();
-var PORT = 3000;
+// Express
+// var app = express();
+// var PORT = 3000;
 
 // Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// Requiring Dependencies.
+var express = require('express')
+var bodyParser = require('body-parser')
+var path = require('path')
+
+// Create an instance of express server.
+var app = express();
+
+// Our port will be whatever we are given, or if given nothing it will be 80.
+var appPORT = process.env.PORT || 80;
+
+// Enabling Express to serve static files. (Allows our JavaScript & CSS files to be used)
+app.use(express.static('app/public'));
+
+// Body-parser middleware!
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+
+// Requiring the files with routing information.
+require('./app/routing/apiRoutes.js')(app); 
+require('./app/routing/htmlRoutes.js')(app);
+
+// Starts our server.
+app.listen(appPORT, function() {
+    console.log("Listening on port: " + appPORT);
+});
